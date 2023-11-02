02d8852f079da376f21831bcad40ed66dfaad739
ayoungt <87863881+ayoungt@users.noreply.github.com>
2023-11-03 00:35:28 +0900

    mobile

--- main.js
+++ main.js
@@ -1,58 +1,58 @@
-
-import {Bodies, Body, Engine, Events, Render, Runner,World} from "matter-js";
-import {FRUITS } from  "./fruits"
+import { Bodies, Body, Engine, Render, Runner, World } from "matter-js";
+import { FRUITS } from "./fruits";
 
 const engine = Engine.create();
 const render = Render.create({
   engine,
-  element: document. body,
+  element: document.body,
   options: {
     wireframes: false,
     background: "#F7F4C8",
     width: 620,
     height: 850,
-  }
+  },
 });
 
 const world = engine.world;
 
-const leftWall = Bodies.rectangle(15,395,30,790, {
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
   isStatic: true,
-  render: {fillStyle: "#E6B143"}
+  render: { fillStyle: "#E6B143" },
 });
 
-const rightWall = Bodies.rectangle(605,395,30,790, {
+const rightWall = Bodies.rectangle(605, 395, 30, 790, {
   isStatic: true,
-  render: {fillStyle: "#E6B143"}
+  render: { fillStyle: "#E6B143" },
 });
 
-const ground = Bodies.rectangle(310,820,620,60, {
+const ground = Bodies.rectangle(310, 820, 620, 60, {
   isStatic: true,
-  render: {fillStyle: "#E6B143"}
+  render: { fillStyle: "#E6B143" },
 });
 
-const topLine = Bodies.rectangle(310,150,620,2, {
+const topLine = Bodies.rectangle(310, 150, 620, 2, {
   name: "topLine",
   isStatic: true,
   isSensor: true,
-  render: {fillStyle: "#E6B143"}
+  render: { fillStyle: "#E6B143" },
 });
 
-World.add(world, [leftWall,rightWall,ground,topLine]);
+World.add(world, [leftWall, rightWall, ground, topLine]);
 
 Render.run(render);
 Runner.run(engine);
 
 let currentBody = null;
 let currentFruit = null;
-let disableAction = null;
+let disableAction = false;
 let interval = null;
 
 function addFruit() {
-  const index = Math.floor(Math.random() * FRUITS.length); // Adjust the range as needed
+  const index = Math.floor(Math.random() * 5);
   const fruit = FRUITS[index];
 
-  const body = Bodies.circle(310, 50, fruit.radius, {
+  const body = Bodies.circle(300, 50, fruit.radius, {
+    index: index,
     isSleeping: true,
     render: {
       sprite: { texture: `${fruit.name}.png` },
@@ -60,54 +60,23 @@ function addFruit() {
     restitution: 0.2,
   });
 
-  World.add(world, body);
-
-  // Add a click event listener to the new fruit
-  body.el = document.createElement("img");
-  body.el.src = `${fruit.name}.png`;
-  body.el.style.position = "absolute";
-  body.el.style.left = `${body.position.x}px`;
-  body.el.style.top = `${body.position.y}px`;
-
-  body.el.addEventListener("click", () => {
-    moveFruit("right"); // You can change this to "left" if you want to move left initially
-  });
-
-  document.body.appendChild(body.el);
-
   currentBody = body;
   currentFruit = fruit;
+
+  World.add(world, body);
 }
 
-function addFruit() {
-  const index = Math.floor(Math.random() * FRUITS.length); // Adjust the range as needed
-  const fruit = FRUITS[index];
+function moveFruit(direction) {
+  if (!currentBody || disableAction) {
+    return;
+  }
 
-  const body = Bodies.circle(310, 50, fruit.radius, {
-    isSleeping: true,
-    render: {
-      sprite: { texture: `${fruit.name}.png` },
-    },
-    restitution: 0.2,
-  });
+  const increment = direction === "left" ? -1 : 1;
+  const newPositionX = currentBody.position.x + increment;
 
-  World.add(world, body);
-
-  // Add a click event listener to the new fruit
-  body.el = document.createElement("img");
-  body.el.src = `${fruit.name}.png`;
-  body.el.style.position = "absolute";
-  body.el.style.left = `${body.position.x}px`;
-  body.el.style.top = `${body.position.y}px`;
-
-  body.el.addEventListener("click", () => {
-    moveFruit("right"); // You can change this to "left" if you want to move left initially
-  });
-
-  document.body.appendChild(body.el);
-
-  currentBody = body;
-  currentFruit = fruit;
+  if (newPositionX >= 30 && newPositionX <= 590) {
+    Body.setPosition(currentBody, { x: newPositionX, y: currentBody.position.y });
+  }
 }
 
 // Event listeners for clicking and touching
@@ -123,13 +92,24 @@ document.addEventListener("click", () => {
   }, 1000);
 });
 
+// Touch events for mobile
+let touchStartX = 0;
+
 document.addEventListener("touchstart", (event) => {
+  touchStartX = event.touches[0].clientX;
+});
+
+document.addEventListener("touchmove", (event) => {
   const touchX = event.touches[0].clientX;
   const screenWidth = window.innerWidth;
-  if (touchX < screenWidth / 2) {
-    moveFruit("left");
-  } else {
+  const moveThreshold = 30;
+
+  if (touchX - touchStartX > moveThreshold) {
     moveFruit("right");
+    touchStartX = touchX;
+  } else if (touchStartX - touchX > moveThreshold) {
+    moveFruit("left");
+    touchStartX = touchX;
   }
 });
 
@@ -137,107 +117,6 @@ document.addEventListener("touchend", () => {
   // Stop moving the fruit when the touch ends
   clearInterval(interval);
   interval = null;
-});
-
-
-
-
-
-window.onkeydown = (event) => {
-  if (disableAction)
-  {
-    return;
-  }
-  switch(event.code)
-  {
-    case "KeyA":
-      if (interval)
-      return;
-
-      interval = setInterval(() => {
-        if (currentBody.position.x - currentFruit.radius > 30)
-        Body.setPosition(currentBody,{
-          x:currentBody.position.x - 1,
-          y: currentBody.position.y,
-        });
-      },);
-      
-      break;
-
-
-    case "KeyD":
-      if (interval)
-        return;
-      
-      interval = setInterval(() => {
-        if (currentBody.position.x + currentFruit.radius < 590)
-        Body.setPosition(currentBody,{
-         x:currentBody.position.x + 1,
-         y: currentBody.position.y,
-        });
-      },5)
-
-      
-      break;
-
-    case "KeyS":
-      currentBody.isSleeping = false;
-      disableAction = true;
-
-      setTimeout(() => {
-        addFruit();  
-        disableAction = false;
-      },1000);
-      
-      break;
-
-  }
-}
-
-window.onkeyup = (event) => {
-  switch (event.code){
-    case "KeyA":
-    case "KeyD":
-        clearInterval(interval);
-        interval = null;
-  }
-}
-
-Events.on(engine, "collisionStart", (event) => {
-  event.pairs.forEach((collision => {
-    if (collision.bodyA.index === collision.bodyB.index) {
-      const index = collision.bodyA.index;
-      if(index === FRUITS.length - 1){
-        return;
-      }
-      
-      World.remove(world, [collision.bodyA, collision.bodyB]);
-
-      const newFruit = FRUITS[index + 1];
-      
-      const newBody = Bodies.circle(
-        collision.collision.supports[0].x,
-        collision.collision.supports[0].y,
-        newFruit.radius,
-        {
-          render: {
-            sprite: {texture: `${newFruit.name}.png`}
-          },
-          index: index + 1,
-        }
-      );
-
-     World.add(world, newBody);
-    }
-
-    if (
-      !disableAction &&
-      (collision.bodyA.name === "topLine" || collision.bodyB.name === "topLine")) {
-      alert("One more time DOPAMIN?");
-    }
-
-
-  }));
 });
 
 addFruit();
